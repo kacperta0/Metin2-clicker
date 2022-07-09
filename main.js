e8688c7ce58bb665c58da163b2ad520f763d0dfb
kacperta0 <kacpertaurzeski@interia.pl>
2022-07-09 14:10:31 +0200

    Created Monster rendering function and now adding functionalities

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
+let monstersArray = [
+  {
+    mName: "Wild dog",
+    mHp: 100,
+  },
+  {
+    mName: "Hungry wild dog",
+    mHp: 120,
+  },
+];
 
+const monsterRender = () => {
+
+  const getMname = document.getElementById("m_name");
+  const getMhp = document.getElementById("m_hp")
+
+  getMname.innerHTML = monstersArray[0].mName;
+  getMhp.innerHTML = monstersArray[0].mHp;
+}
+
+monsterRender();
