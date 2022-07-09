8718051165977ac37248b18d1e67b21bc544306b
kacperta0 <kacpertaurzeski@interia.pl>
2022-07-09 17:01:31 +0200

    Created function that renders random monster

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+/* 
+
+1. Create array with monsters that includes name, health points, experience etc.
+
+2. Create function that put out random monster by number
+
+3. Create function that renders the whole game, monster name, health points
+
+4. Create attack function, that changes monster Health Points depending on players attack DMG.
+
+5. Create experience system, player leveling.
+
+*/
+
+
+let mNumber = 0;
+
+// 1. Array of monsters -----------------
 let monstersArray = [
   {
     mName: "Wild dog",
@@ -9,13 +27,24 @@ let monstersArray = [
   },
 ];
 
-const monsterRender = () => {
+
+
+
+const gameRender = () => {
+
+  // 2. Random monster by number
+  mNumber = Math.floor(Math.random() * monstersArray.length); 
 
   const getMname = document.getElementById("m_name");
-  const getMhp = document.getElementById("m_hp")
+  const getMhp = document.getElementById("m_hp");
+  const getAttackButton = document.getElementById("m_attack");
 
-  getMname.innerHTML = monstersArray[0].mName;
-  getMhp.innerHTML = monstersArray[0].mHp;
+  getMname.innerHTML = monstersArray[mNumber].mName;
+  getMhp.innerHTML = monstersArray[mNumber].mHp;
+  getAttackButton.addEventListener("click", playerAttack);
 }
 
-monsterRender();
+const playerAttack = () => {
+
+}
+gameRender();
